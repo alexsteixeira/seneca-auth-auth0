c5a509abf4766513eeafdf33821aa828866c0282
Alexsandro Teixeira <alexsandrot@gmail.com>
2016-08-11 20:46:39 -0300

    name fix + scope

--- auth0-auth.js
+++ auth0-auth.js
@@ -1,22 +1,27 @@
-var Auth0Strategy = require('passport-auth0')
 
-module.exports = function(opt) {
-    var plugin = new Auth0Strategy({
-        domain: opt.domain,
-        clientID: opt.clientID,
-        clientSecret: opt.clientSecret,
-        callbackURL: opt.callbackURL
-    }),
+var Auth0Strategy = require('passport-auth0');
+
+module.exports = function (options) {
+
+  var seneca = this
+
+  var authPlugin = new Auth0Strategy({
+      domain: options.domain,
+      clientID:    options.apiKey,
+      clientSecret: options.apiSecret,
+      callbackURL:    options.urlhost + "/callback"
+    },
     function(accessToken, refreshToken, extraParams, profile, done) {
-        // accessToken is the token to call Auth0 API
+        // accessToken is the token to call Auth0 API (not needed in the most cases)
         // extraParams.id_token has the JSON Web Token
         // profile has all the information from the user
         return done(null, profile);
     }
+  )
 
-    this.act({role: 'auth', cmd: 'register_service', service: 'auth0', plugin: plugin, conf: opt})
+  seneca.act({role: 'auth', cmd: 'register_service', service: 'auth0', plugin: authPlugin, conf: options})
 
-    return {
-        name: 'auth0-auth'
-    }
+  return {
+    name: 'auth0-auth'
+  }
 }
